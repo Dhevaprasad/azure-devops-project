84edc4baa344142d0481a39eec29ba139980e0d2
Dhevaprasad <dhevaprasad1604@gmail.com>
2026-08-20 22:28:30 +0530

    Initial project setup

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  type    = string
+  default = "Central India"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "rg-devops-project"
+}
+
+variable "acr_name" {
+  type    = string
+  default = "acrdevopsproject2026"
+}
+
+variable "aks_name" {
+  type    = string
+  default = "aks-devops-project"
+}
